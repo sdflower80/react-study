346f125189ffff839ef275d7a6914e1d7673a4cd
sdflower <sdflower@coupang.com>
2022-01-03 01:07:42 +0900

    AddTask, UI, useState

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -4,7 +4,7 @@ import Tasks from "./Tasks";
 import AddTask from "./AddTask";
 
 function App() {
-
+    const [showAddTask, setShowAddTask] = useState(false)
     const [tasks, setTasks] = useState([
         {
             id: 1,
@@ -26,6 +26,12 @@ function App() {
         },
     ])
 
+    const addTask = (task) => {
+        const id = Math.floor(Math.random() * 10000) + 1
+        const newTask = { id, ...task }
+        setTasks([...tasks, newTask])
+    }
+
     const deleteTask = (id) => {
         setTasks(tasks.filter((task) =>
             task.id !== id
@@ -41,8 +47,9 @@ function App() {
 
     return (
         <div className="container">
-            <Header/>
-            <AddTask />
+            <Header onAdd={() => setShowAddTask(!showAddTask)}
+                    showAdd={showAddTask} />
+            {showAddTask && <AddTask onAdd={addTask} />}
             {tasks.length > 0 ?
                 (<Tasks tasks={ tasks }
                         onDelete={deleteTask}
